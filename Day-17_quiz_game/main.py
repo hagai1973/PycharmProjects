1cb690ded2ba682f35f1a29383879707908c7a79
Hagai Tregerman <hagai1973@gmail.com>
2022-05-04 11:42:13 +0300

    D-17_Quiz

--- Day-17_quiz_game/main.py
+++ Day-17_quiz_game/main.py
@@ -3,11 +3,6 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
-# q1 = Question("2+2=4", "True")
-#
-# print(f"question:  {q1.text}, answer: {q1.answer}")
-
-
 question_bank = []
 
 for question in question_data:
@@ -16,10 +11,13 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
+
 game = QuizBrain(question_bank)
-print(game.game_round())
+while game.still_has_question():
+    (game.game_round())
 
-
+print(f"You've completed the quiz")
+print(f"Your final score was: {game.score}/{len(game.q_list)} ")
 
 
 
--- Day-17_quiz_game/quiz_brain.py
+++ Day-17_quiz_game/quiz_brain.py
@@ -2,19 +2,34 @@ class QuizBrain:
     def __init__(self, question_list):
         self.q_number = 0
         self.q_list = question_list
+        self.score = 0
 
     def next_question(self, item):
         q = self.q_list[self.q_number].text
-        q_response = input(f"Q.{self.q_number + 1}: {q} (True/False)?: \n").lower()
-        if str(item.answer).lower() == q_response.lower():
+        question_answer = self.q_list[self.q_number].answer
+        self.q_number += 1
+        user_answer = input(f"Q.{self.q_number}: {q} (True/False)?: \n").lower()
+        return self.check_answer(user_answer, question_answer)
+
+    def game_round(self):
+        greating = "let's continue"
+        if self.q_number == len(self.q_list) - 1:
+            greating = "Quiz completed"
+        if self.next_question(self.q_list[self.q_number]):
+            print(f"Good job, your score is: {self.score}/{len(self.q_list)}, {greating}")
+            # return True
+        else:
+            print(f"Wrong answer...your score is: {self.score}/{len(self.q_list)}, lets continue")
+            # return False
+        print("\n")
+
+    def check_answer(self, user_answer, question_answer):
+        print(f"the correct answer is {question_answer}")
+        if user_answer.lower() == question_answer.lower():
+            self.score += 1
             return True
         else:
             return False
 
-    def game_round(self):
-        if self.next_question(self.q_list[self.q_number]):
-            print("good answer, lets continue")
-            return True
-        else:
-            print("wrong answer, game over")
-            return False
+    def still_has_question(self):
+        return self.q_number < len(self.q_list)
