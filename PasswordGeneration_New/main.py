c6a50237aa2f5ac9d198c9998b012fa69f6ce128
Hagai Tregerman <hagai.tregerman@forcepoint.com>
2023-04-12 00:13:18 +0300

    add data.txt

--- PasswordGeneration_New/main.py
+++ PasswordGeneration_New/main.py
@@ -1,8 +1,43 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def genrate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    print(f"Your password is: {password}")
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -56,7 +91,7 @@ password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1, columnspan=2, sticky=W, padx=10)
 
 # Buttons
-genrate_password_button = Button(text='G', width=2, padx=10)
+genrate_password_button = Button(text='G', width=2, padx=10, command=genrate_password)
 genrate_password_button.grid(row=3, column=2, sticky=W)
 add_button = Button(text='Add', width=32, command=save)
 add_button.grid(row=4, column=1, columnspan=2, padx=1, pady=10)
